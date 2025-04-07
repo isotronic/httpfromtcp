28ab9c260b827093c57f90db47b3a3e7532c775a
Joseph Bouqdib <joseph@bouqdib.com>
2025-04-07 20:39:32 +0100

    feat: parse request data as it is received instead of all at once

--- internal/request/request.go
+++ internal/request/request.go
@@ -11,6 +11,7 @@ type Request struct {
 	RequestLine RequestLine
 	Headers     map[string]string
 	Body        []byte
+	State       int
 }
 
 type RequestLine struct {
@@ -19,41 +20,87 @@ type RequestLine struct {
 	Method        string
 }
 
+const (
+	initialized = iota
+	done
+)
+
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	reqLine, err := parseRequestLine(data)
-	if err != nil {
-		return nil, err
-	}
-
+	buf := make([]byte, bufferSize)
+	readToIndex := 0
 	req := Request{
-		RequestLine: *reqLine,
+		State: initialized,
+	}
+
+	for {
+		if req.State == done {
+			break
+		}
+
+		// Double the buffer size if it's full
+		if len(buf) == readToIndex {
+			newBuffer := make([]byte, len(buf)*2)
+			copy(newBuffer, buf)
+			buf = newBuffer
+		}
+
+		// Read from the reader into the buffer
+		n, err := reader.Read(buf[readToIndex:])
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return &req, err
+		}
+		// Increment the readToIndex to show how many bytes have been read
+		readToIndex += n
+
+		// Parse the buffer
+		parsedBytes, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return &req, err
+		}
+
+		// Shift the buffer to the left
+		copy(buf, buf[parsedBytes:readToIndex])
+
+		// Decrement the readToIndex to show remaining unparsed bytes
+		readToIndex -= parsedBytes
+	}
+	
+	// Handle any remaining bytes in the buffer
+	if readToIndex > 0 {
+		_, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return &req, err
+		}
 	}
 
 	return &req, nil
 }
 
-func parseRequestLine(b []byte) (*RequestLine, error) {
+// parseRequestLine parses the request line and returns the number of bytes read
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
 	reqLine := &RequestLine{}
 	str := string(b)
-	line := strings.Split(str, "\r\n")[0]
-	parts := strings.Split(line, " ")
+	lines := strings.Split(str, "\r\n")
+	if len(lines) < 2 {
+		return reqLine, 0, nil
+	}
+	parts := strings.Split(lines[0], " ")
 	if len(parts) != 3 {
-		return reqLine , fmt.Errorf("invalid number of parts in request line: %s", line)
+		return reqLine, 0, fmt.Errorf("invalid number of parts in request line: %s", lines)
 	}
 
 	for _, char := range parts[0] {
 		if !unicode.IsUpper(char) {
-			return reqLine, fmt.Errorf("invalid method: %s", parts[0])
+			return reqLine, 0, fmt.Errorf("invalid method: %s", parts[0])
 		}
 	}
 
 	if parts[2] != "HTTP/1.1" {
-		return reqLine, fmt.Errorf("invalid http version: %s", parts[2])
+		return reqLine, 0, fmt.Errorf("invalid http version: %s", parts[2])
 	}
 
 	versionParts := strings.Split(parts[2], "/")
@@ -62,5 +109,27 @@ func parseRequestLine(b []byte) (*RequestLine, error) {
 	reqLine.RequestTarget = parts[1]
 	reqLine.HttpVersion = versionParts[1]
 
-	return reqLine, nil
+	return reqLine, len(lines[0]) + 2, nil
+}
+
+// parse parses the request line and sets the state to done
+func (r *Request) parse(data []byte) (int, error) {
+	if r.State == initialized {
+		reqLine, numBytesPerRead, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+
+		if numBytesPerRead == 0 {
+			return 0, nil
+		}
+
+		r.RequestLine = *reqLine
+		r.State = done
+		return numBytesPerRead, nil
+	} else if r.State == done {
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	} else {
+		return 0, fmt.Errorf("error: unknown state")
+	}
 }
